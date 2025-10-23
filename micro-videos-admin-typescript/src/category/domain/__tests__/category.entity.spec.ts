fb14a83dc9f6cf098426346238797afde1ca2050
Lucas Chaves <67830005+lucasmar236@users.noreply.github.com>
2025-10-23 09:45:45 -0300

    test: add valitador tests

--- micro-videos-admin-typescript/src/category/domain/__tests__/category.entity.spec.ts
+++ micro-videos-admin-typescript/src/category/domain/__tests__/category.entity.spec.ts
@@ -158,6 +158,79 @@ describe('Category Validator', () => {
                         "name must be shorter than or equal to 255 characters"]
                 }
             )
+            expect(() =>
+                Category.create({
+                    name: ""
+                })).containsErrorMessages(
+                {
+                    name: ["name should not be empty"]
+                }
+            )
+            expect(() =>
+                Category.create({
+                    name: 5 as any
+                })).containsErrorMessages(
+                {"name":["name must be a string","name must be shorter than or equal to 255 characters"]}
+            )
+            expect(() =>
+                Category.create({
+                    name: "t".repeat(256)
+                })).containsErrorMessages(
+                {
+                    name: ["name must be shorter than or equal to 255 characters"]
+                }
+            )
+            expect(() =>
+                Category.create({
+                    description : 5
+                } as any)).containsErrorMessages(
+                {"name":["name should not be empty","name must be a string",
+                        "name must be shorter than or equal to 255 characters"],
+                    "description":["description must be a string"]}
+            )
+
+            expect(() =>
+                Category.create({ is_active : 5 } as any)).containsErrorMessages(
+                {"name":["name should not be empty","name must be a string",
+                        "name must be shorter than or equal to 255 characters"],
+                    "is_active":["is_active must be a boolean value"]}
+            )
         })
     })
+    describe("changeName method", () => {
+        it("should a invalid category using name property", () => {
+            const category = Category.create({ name: "Movie" });
+            expect(() => category.changeName(null)).containsErrorMessages({
+                name: [
+                    "name should not be empty",
+                    "name must be a string",
+                    "name must be shorter than or equal to 255 characters",
+                ],
+            });
+
+            expect(() => category.changeName("")).containsErrorMessages({
+                name: ["name should not be empty"],
+            });
+
+            expect(() => category.changeName(5 as any)).containsErrorMessages({
+                name: [
+                    "name must be a string",
+                    "name must be shorter than or equal to 255 characters",
+                ],
+            });
+
+            expect(() => category.changeName("t".repeat(256))).containsErrorMessages({
+                name: ["name must be shorter than or equal to 255 characters"],
+            });
+        });
+    });
+
+    describe("changeDescription method", () => {
+        it("should a invalid category using description property", () => {
+            const category = Category.create({ name: "Movie" });
+            expect(() => category.changeDescription(5 as any)).containsErrorMessages({
+                description: ["description must be a string"],
+            });
+        });
+    });
 })
